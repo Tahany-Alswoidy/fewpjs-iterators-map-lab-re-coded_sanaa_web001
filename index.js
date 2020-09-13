2670c91505d4a7068573746e6c0b5c75f91b896d
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-13 07:06:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const titleCased = () => {
     sentence=sentence.split(" ");
     let newSentence="";
     for(let word of sentence){
-      newword=word.replace(word.charAt(0),word.toUpperCase(word.charAt(0)));
+      let newword=word.replace(word.charAt(0),word.toUpperCase(word.charAt(0)));
       newSentence+=newword+" ";
     }
     newtutorials.push(newSentence);
